da644ee808d978ba3a64179669073fb26d070ab7
lanseria <zhangchao564265135@hotmail.com>
2022-10-25 22:25:16 +0800

    数据结构完成

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,22 +3,32 @@ import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@Schema()
+@Schema({
+  timestamps: {
+    currentTime: () => Math.floor(Date.now() / 1000),
+  },
+})
 export class User extends Document {
-  @Prop({ required: true })
-  name: string;
-
-  @Prop()
-  age: number;
-
-  @Prop()
+  @Prop({ required: true, type: String })
   openid: string;
 
-  @Prop([String])
-  tags: string[];
-
-  @Prop()
+  @Prop({ required: true, type: Boolean })
   isSubscribe: boolean;
+
+  @Prop({ required: true, type: Number })
+  createdAt: number;
+
+  @Prop({ required: true, type: Number })
+  updatedAt: number;
+
+  @Prop({ required: true, type: Number })
+  currentSampleDateTime: number;
+
+  @Prop({ required: true, type: Number })
+  intervalDay: number;
+
+  @Prop({ required: true, type: Number })
+  nextSampleDateTime: number;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
